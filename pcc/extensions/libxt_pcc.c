6c4c5341c179fa138cf6b25b65279be3f8bf35d3
cary.zhou <kaichuangluo@gmail.com>
2018-07-29 05:28:46 +0800

    Update libxt_pcc.c
    新增源端口与目标端口配置选项

--- pcc/extensions/libxt_pcc.c
+++ pcc/extensions/libxt_pcc.c
@@ -1,12 +1,3 @@
-/*
- *	"pcc" match extension for iptables
- *	2015 Tiago Marques <tmarques@viaconnect.com.br>
- *
- *	This program is free software; you can redistribute it and/or
- *	modify it under the terms of the GNU General Public License; either
- *	version 2 of the License, or any later version, as published by the
- *	Free Software Foundation.
- */
 #include <getopt.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -17,41 +8,50 @@
 #include "compat_user.h"
 
 static const struct option pcc_mt_opts[] = {
-	{.name = "pcc-src",   .has_arg = false, .val = 's'},
-	{.name = "pcc-dst",   .has_arg = false, .val = 'd'},
-	{.name = "pcc-mod",   .has_arg = true,  .val = 'm'},
-	{.name = "pcc-value", .has_arg = true,  .val = 'v'},
+	{.name = "src-addr",   .has_arg = false, .val = 's'},
+	{.name = "dst-addr",   .has_arg = false, .val = 'd'},
+	{.name = "src-port",   .has_arg = false, .val = 'x'},
+	{.name = "dst-port",   .has_arg = false, .val = 'y'},
+	{.name = "pcc-mod",    .has_arg = true,  .val = 'm'},
+	{.name = "pcc-value",  .has_arg = true,  .val = 'v'},
 	{NULL},
 };
 
-static void pcc_mt_help(void)
-{
+static void pcc_mt_help(void){
 	printf(
 	"pcc match options:\n"
-	"    --pcc-src           Include IPv4/IPv6 source address to hash calculation\n"
-	"    --pcc-dst           Include IPv4/IPv6 destination address to hash calculation\n"
-	"    --pcc-mod modulus   Modulus for hash calculation (to limit the range of possible values)\n"
-	"[!] --pcc-value value   Value to compare\n"
+	"    --src-addr           Include IPv4/IPv6 source address to hash calculation\n"
+	"    --dst-addr           Include IPv4/IPv6 destination address to hash calculation\n"
+	"    --src-port           Include IPv4/IPv6 source port to hash calculation\n"
+	"    --dst-port           Include IPv4/IPv6 destination port to hash calculation\n"
+	"    --pcc-mod modulus    Modulus for hash calculation (to limit the range of possible values)\n"
+	"[!] --pcc-value value    Value to compare\n"
 	);
 }
 
-static int
-pcc_mt_parse(int c, char **argv, int invert, unsigned int *flags,
-	        const void *entry, struct xt_entry_match **match)
-{
+static int pcc_mt_parse(int c, char **argv, int invert, unsigned int *flags, const void *entry, struct xt_entry_match **match){
 	struct xt_pcc_mtinfo *info = (void *)(*match)->data;
 	char *endptr;
-
 	switch (c) {
 	case 's':
-		xtables_param_act(XTF_ONLY_ONCE, "pcc", "--pcc-src", *flags & XT_PCC_SRC);
-		*flags      |= XT_PCC_SRC;
-		info->flags |= XT_PCC_SRC;
+		xtables_param_act(XTF_ONLY_ONCE, "pcc", "--src-addr", *flags & XT_SRC_ADDR);
+		*flags      |= XT_SRC_ADDR;
+		info->flags |= XT_SRC_ADDR;
 		return true;
 	case 'd':
-		xtables_param_act(XTF_ONLY_ONCE, "pcc", "--pcc-dst", *flags & XT_PCC_DST);
-		*flags      |= XT_PCC_DST;
-		info->flags |= XT_PCC_DST;
+		xtables_param_act(XTF_ONLY_ONCE, "pcc", "--dst-addr", *flags & XT_DST_ADDR);
+		*flags      |= XT_DST_ADDR;
+		info->flags |= XT_DST_ADDR;
+		return true;
+	case 'x':
+		xtables_param_act(XTF_ONLY_ONCE, "pcc", "--src-port", *flags & XT_SRC_PORT);
+		*flags      |= XT_SRC_PORT;
+		info->flags |= XT_SRC_PORT;
+		return true;
+	case 'y':
+		xtables_param_act(XTF_ONLY_ONCE, "pcc", "--dst-port", *flags & XT_DST_PORT);
+		*flags      |= XT_DST_PORT;
+		info->flags |= XT_DST_PORT;
 		return true;
 	case 'm':
 		xtables_param_act(XTF_ONLY_ONCE, "pcc", "--pcc-mod", *flags & XT_PCC_MOD);
@@ -59,8 +59,7 @@ pcc_mt_parse(int c, char **argv, int invert, unsigned int *flags,
 		info->flags |= XT_PCC_MOD;
 		info->mod = strtoull(optarg, &endptr, 0);
 		if (*endptr != '\0' || !info->mod)
-			xtables_error(PARAMETER_PROBLEM, "pcc match: "
-			           "invalid value for --pcc-mod");
+			xtables_error(PARAMETER_PROBLEM, "pcc match: invalid value for --pcc-mod");
 		return true;
 	case 'v':
 		xtables_param_act(XTF_ONLY_ONCE, "pcc", "--pcc-value", *flags & XT_PCC_VALUE);
@@ -70,22 +69,22 @@ pcc_mt_parse(int c, char **argv, int invert, unsigned int *flags,
 			info->flags |= XT_PCC_INVERT;
 		info->value = strtoul(optarg, &endptr, 0);
 		if (*endptr != '\0')
-			xtables_error(PARAMETER_PROBLEM, "pcc match: "
-			           "invalid value for --pcc-value");
+			xtables_error(PARAMETER_PROBLEM, "pcc match: invalid value for --pcc-value");
 		return true;
 	}
 	return false;
 }
 
-static void
-pcc_mt_save(const void *ip, const struct xt_entry_match *match)
-{
+static void pcc_mt_save(const void *ip, const struct xt_entry_match *match){
 	const struct xt_pcc_mtinfo *info = (void *)match->data;
-
-	if (info->flags & XT_PCC_SRC)
-		printf(" --pcc-src");
-	if (info->flags & XT_PCC_DST)
-		printf(" --pcc-dst");
+	if (info->flags & XT_SRC_ADDR)
+		printf(" --src-addr");
+	if (info->flags & XT_DST_ADDR)
+		printf(" --dst-addr");
+	if (info->flags & XT_SRC_PORT)
+		printf(" --src-port");
+	if (info->flags & XT_DST_PORT)
+		printf(" --dst-port");
 	if (info->flags & XT_PCC_MOD)
 		printf(" --pcc-mod %lu", (unsigned long)info->mod);
 	if (info->flags & XT_PCC_INVERT)
@@ -94,9 +93,7 @@ pcc_mt_save(const void *ip, const struct xt_entry_match *match)
 		printf(" --pcc-value %lu", (unsigned long)info->value);
 }
 
-static void pcc_mt_print(const void *ip, const struct xt_entry_match *match,
-                            int numeric)
-{
+static void pcc_mt_print(const void *ip, const struct xt_entry_match *match, int numeric){
 	printf(" -m pcc");
 	pcc_mt_save(ip, match);
 }
@@ -115,7 +112,6 @@ static struct xtables_match pcc_mt_reg = {
 	.extra_opts    = pcc_mt_opts,
 };
 
-static __attribute__((constructor)) void pcc_mt_ldr(void)
-{
+static __attribute__((constructor)) void pcc_mt_ldr(void){
 	xtables_register_match(&pcc_mt_reg);
 }
