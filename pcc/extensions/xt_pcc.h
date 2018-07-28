5d1d3db4093c606368c1e286527dda1bec3a7c96
cary.zhou <kaichuangluo@gmail.com>
2018-07-29 05:31:45 +0800

    Update xt_pcc.h
    枚举常量新增源端口与目标端口

--- pcc/extensions/xt_pcc.h
+++ pcc/extensions/xt_pcc.h
@@ -2,11 +2,13 @@
 #define _XT_PCC_H
 
 enum xt_pcc_flags {
-	XT_PCC_INVERT = 1 << 0,
-	XT_PCC_SRC    = 1 << 1,
-	XT_PCC_DST    = 1 << 2,
-	XT_PCC_MOD    = 1 << 3,
-	XT_PCC_VALUE  = 1 << 4,
+	XT_PCC_INVERT  = 1 << 0,
+	XT_SRC_ADDR    = 1 << 1,
+	XT_DST_ADDR    = 1 << 2,
+	XT_SRC_PORT    = 1 << 3,
+	XT_DST_PORT    = 1 << 4,
+	XT_PCC_MOD     = 1 << 5,
+	XT_PCC_VALUE   = 1 << 6,
 };
 
 struct xt_pcc_mtinfo {
@@ -14,4 +16,4 @@ struct xt_pcc_mtinfo {
 	u_int32_t mod, value;
 };
 
-#endif /* _XT_PCC_H */
+#endif
